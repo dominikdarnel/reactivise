dc9195107528d1e9be6f1e650ab8007c2cbdaa42
dominikdarnel <dominik.darnel@gmail.com>
2019-12-24 10:20:13 +0100

    Restructure app folders

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import BsContainer from "react-bootstrap/Container";
-import Navbar from "./Navbar";
-import ClientsPage from "./ClientsPage";
+import Navbar from "../navbar";
+import ClientsPage from "../clients-page";
 
 function App() {
   return (
