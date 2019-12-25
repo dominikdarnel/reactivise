6c54ddc022c6461460fe534b889c805788dcb167
dominikdarnel <dominik.darnel@gmail.com>
2019-12-25 20:04:04 +0100

    Header label for modal

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -2,21 +2,21 @@ import React, { useState, useEffect } from "react";
 import BsModal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 
-function Modal({ open, buttonLabel, children }) {
+function Modal({ open, buttonLabel, headerLabel, children }) {
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
 
   useEffect(() => {
     setShow(open);
-  }, []);
+  }, [open]);
 
   return (
     <>
       <div data-testid="modal" />
       <BsModal show={show} onHide={handleClose}>
         <BsModal.Header closeButton>
-          <BsModal.Title>BsModal heading</BsModal.Title>
+          <BsModal.Title>{headerLabel}</BsModal.Title>
         </BsModal.Header>
         <BsModal.Body data-testid="modal-body">{children}</BsModal.Body>
         <BsModal.Footer>
